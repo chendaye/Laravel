dc3ed24c747e2518b6f68ec28da7af12d0ff32ae
chendaye <chendaye666@gmail.com>
2017-06-13 20:27:09 +0800

    rm extra file

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-
+        return view('login.index');
     }
 
     public function login()
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,13 +6,21 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
+    /**
+     * 注册页面
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
-
+        //展示注册页面
+        return view('register.index');
     }
 
     public function register()
     {
-
+        //表单验证
+        $this->validate([]);
+        //逻辑
+        //渲染
     }
 }
