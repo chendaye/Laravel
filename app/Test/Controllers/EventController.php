e891ec9a2d4459be79ff4e0f2213014636321b0f
chendaye <chendaye666@gmail.com>
2017-07-22 07:16:23 +0800

    start

--- app/Test/Controllers/EventController.php
+++ app/Test/Controllers/EventController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Test\Controllers;
+use App\Events\EventTest;
 
 /**
  * 事件系统
@@ -30,6 +31,7 @@ class EventController extends Controller
      * 当然已经存在的事件和监听器将保持不变：
      *
      * php artisan event:generate
+     * 此命令会自动生成已经注册的 Event 和 Listener 文件 并引入适当的命名空间
      */
 
 
@@ -43,6 +45,52 @@ class EventController extends Controller
      * 定义事件
      * 事件类就是一个包含与事件相关信息数据的容器。
      * 例如，假设我们生成的 OrderShipped 事件接受一个 Eloquent ORM 对象*/
+
+    /*
+     * 事件的监听器。
+     * 事件监听器在 handle 方法中接受了事件实例作为参数。
+     * event:generate 命令将会在事件的 handle 方法中自动加载正确的事件类和类型提示。
+     * 在 handle 方法中，你可以运行任何需要响应该事件的业务逻辑*/
+
+
+    /*
+     * 队列化事件监听器
+     * 如果你的监听器中需要实现一些耗时的任务，比如发送邮件或者进行 HTTP 请求，
+     * 那把它放到队列中处理是非常有用的。
+     * 在使用队列化监听器之前，一定要在服务器或者本地环境中配置 队列 并开启一个队列监听器。
+     * 要对监听器进行队列化的话，只需增加 ShouldQueue 接口到你的监听器类。
+     * 由 Artisan 命令  event:generate 生成的监听器已经将此接口导入到命名空间了，因此你可以直接使用它*/
+
+
+    /*
+     * 处理失败任务
+     * 有时你队列化的事件监听器可能失败了。
+     * 如果队列监听器任务执行次数超过在工作队列中定义的最大尝试次数，
+     * 监听器的 failed 方法将会被自动调用。 failed 方法接受事件实例和失败的异常作为参数*/
+
+    /*
+     * 触发事件
+     * 如果要触发事件，你可以传递一个事件实例给 event 辅助函数。
+     * 这个函数将会把事件分发到它所有已经注册的监听器上。
+     * 因为 event 函数是全局可访问的，所以你可以在应用中的任何地方调用它*/
+
+    /**
+     * 将传递过来的订单发货。
+     *触发事件
+     * 如果要触发事件，你可以传递一个事件实例给 event 辅助函数。
+     * 这个函数将会把事件分发到它所有已经注册的监听器上。
+     * 因为 event 函数是全局可访问的，所以你可以在应用中的任何地方调用它
+     *
+     * @param  int  $orderId
+     * @return Response
+     */
+    public function ship($orderId)
+    {
+        $order = Order::findOrFail($orderId);
+
+        // 订单的发货逻辑...  触发一个事件 事件调用其监听器处理 任务
+        event(new EventTest($order));
+    }
 
 }
 ?>
